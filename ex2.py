89133e19a76bd31c6f796a58d3a53908bc7646a3
Ryan Erwin <ryan.erwin1@gmail.com>
2015-12-28 17:22:41 -0600

    First commit

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# a comment, this is so you can read your program later.
+# anything after the # is ignored
+
+print "I could have code like this." # and the comment is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run"
+
+print "This will run."

ab81cb3302611ee2304e69ad02bd8a8a985d2ab2
Ryan Erwin <ryan.erwin1@gmail.com>
2015-12-29 02:11:40 -0600

    ex 10 and 11

---
+++ ex10_b.py
@@ -0,0 +1,20 @@
+# testing out triple quotes as well
+test = '''
+Here we go.
+I'm trying triple single quotes.
+Let's print.
+'''
+
+print test
+
+
+filler = "...and..."
+
+print filler
+
+test2 = """
+Triple doubles again.
+Let's print.
+"""
+
+print test2
